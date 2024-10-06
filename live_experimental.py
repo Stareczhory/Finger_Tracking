85f1743ae6f60eb95d14d52ca78aac6b46a8acc1
Jakub Lach <jakub.lach@gmail.com>
2024-10-06 14:08:59 +0200

    program now outputs the angle in degrees for each individual finger

--- live_experimental.py
+++ live_experimental.py
@@ -5,6 +5,15 @@ from mediapipe.tasks.python import vision
 import handle_image
 import time
 import math
+from collections import deque
+import datetime
+import copy
+import csv
+
+buffer_size = 500
+full_data_stream = deque(maxlen=buffer_size)
+filename = 'tracking_data.csv'
+
 
 VisionRunningMode = mp.tasks.vision.RunningMode
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
@@ -13,6 +22,13 @@ current_frame = None
 coordinates = None
 cords_num_array = [8, 12, 16, 20]
 cords_angles = []
+
+def save_data_to_csv(data_deque, file):
+    with open(filename, 'w', newline='') as csvfile:
+        csv_write = csv.writer(csvfile)
+        for row in data_deque:
+            csv_write.writerow(row)
+        data_deque.clear()
 
 def calculate_angle(cords: HandLandmarkerResult):
     hand_landmarker_result = cords
@@ -30,7 +46,7 @@ def calculate_angle(cords: HandLandmarkerResult):
             # length from endpoint to origin
             length_d = math.sqrt((coordinate_values[8+num_counter].z - coordinate_values[0].z)**2 + (coordinate_values[8+num_counter].y - coordinate_values[0].y)**2)
             angle_cb = math.acos((length_d**2 - length_b**2 - length_c**2)/-(2*length_c*length_b))
-            cords_angles.append(round(math.degrees(angle_cb), 2))
+            cords_angles.append(int(math.degrees(angle_cb)))
             num_counter += 4
 # Create a hand landmarker instance with the live stream mode:
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
@@ -48,32 +64,39 @@ options = vision.HandLandmarkerOptions(base_options=base_options,
 detector = vision.HandLandmarker.create_from_options(options)
 
 cap = cv.VideoCapture(0)
+try:
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-while cap.isOpened():
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+        # reset the 2-dimensional list
+        # get current time that past in ms (last three digits)
+        timestamp = int(time.time() * 1000)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+        detector.detect_async(mp_image, timestamp)
+        if current_frame is not None:
+            cv.imshow('Hand Tracking', current_frame)
+            cords_angles = []
+            calculate_angle(coordinates)
+            cords_angles.append(timestamp)
+            full_data_stream.append(copy.deepcopy(cords_angles))
+            if len(full_data_stream) >= buffer_size:
+                save_data_to_csv(full_data_stream, filename)
 
-    # reset the 2-dimensional list
-    # get current time that past in ms (last three digits)
-    timestamp = int(time.time() * 1000)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame, cv.COLOR_BGR2RGB))
-    detector.detect_async(mp_image, timestamp)
-    if current_frame is not None:
-        cv.imshow('Hand Tracking', current_frame)
-        cords_angles = []
-        calculate_angle(coordinates)
-        print(cords_angles)
+        else:
+            cv.imshow('Hand Tracking', frame)
 
-    else:
-        cv.imshow('Hand Tracking', frame)
-
-    if cv.waitKey(1) == ord('q'):
-        break
-
+        if cv.waitKey(1) == ord('q'):
+            break
+except KeyboardInterrupt:
+    # Handle graceful shutdown on user interruption (Ctrl+C)
+    print("Data stream interrupted by user. Saving remaining data...")
+    if full_data_stream:
+        save_data_to_csv(full_data_stream, filename)
 cap.release()
 cv.destroyAllWindows()
